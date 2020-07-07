1c11ec9a4936bf7b59f81ec5d1fbc306c83c1751
Giovanni Mendoza <geoman025@gmail.com>
2020-07-07 07:56:07 -0700

    Added Blog Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+# import datetime
 
-# Create your models here.
+# Create a Blog model
+# title
+# pub_date
+# body
+# image
+
+# Add blog app to settings
+
+# Create migration and then migrate
+
+# Add to admin
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=120)
+    # pub_date = datetime.datetime.now().strftime("%m/%d/%Y")
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    #path('', views.homepage, name='home')
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
